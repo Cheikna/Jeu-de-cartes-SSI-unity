7d0db76c319828611809fa311312ce205f651591
Cheikna <cheikna.dansoko@outlook.fr>
2018-09-23 16:37:29 +0200

    Ajout d'un menu principale
    - Ajout d'un menu qui permet de voir les Crédits, de jouer ou bien de quitter le jeu.

---
+++ Assets/Scripts/ShowHideCanvas.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHideCanvas : MonoBehaviour {
+
+    [SerializeField]
+    private Canvas mainMenuCanvas;
+    [SerializeField]
+    private Canvas creditsCanvas;
+    [SerializeField]
+    private Canvas quitCanvas;
+
+    // Use this for initialization
+    void Start () {
+        creditsCanvas.enabled = false;
+        quitCanvas.enabled = false;
+        mainMenuCanvas.enabled = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void showHideCanvas(string nextCanvasToShow)
+    {
+        switch(nextCanvasToShow)
+        {
+            case "credits":
+                creditsCanvas.enabled = true;
+                quitCanvas.enabled = false;
+                mainMenuCanvas.enabled = false;
+                break;
+
+            case "menu":
+                creditsCanvas.enabled = false;
+                quitCanvas.enabled = false;
+                mainMenuCanvas.enabled = true;
+                break;
+
+            case "quit":
+                creditsCanvas.enabled = false;
+                quitCanvas.enabled = true;
+                mainMenuCanvas.enabled = false;
+                break;
+        }
+    }
+}
